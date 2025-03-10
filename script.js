dc2789e7383dd894d9c3acfc908e520451ae58b7
JuanAndrda <juanmiguelrashley.andrada2005@gmail.com>
2025-03-10 15:59:22 +0800

    Updated files with new versions

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Smart Riding Website Loaded!");
+// JavaScript to change the image source on scroll
+document.addEventListener("scroll", function() {
+    console.log("Scrolling detected"); // Debugging statement
+  
+    var placeholderImage = document.getElementById("placeholder-image");
+  
+    if (window.scrollY > 100) { // Adjust the scroll position as needed
+      console.log("Scroll position > 100px, changing image to bridge.png"); // Debugging statement
+      placeholderImage.src = "bridge.png";
+    } else {
+      console.log("Scroll position < 100px, changing image to pic1.png"); // Debugging statement
+      placeholderImage.src = "pic1.png";
+    }
+  });
+  
